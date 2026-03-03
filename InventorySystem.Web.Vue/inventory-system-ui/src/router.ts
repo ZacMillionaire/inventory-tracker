96cdcab95965f06f34d81502d7620cfd98c476c6
Scott <scott.schultz@jotunga.com>
2026-03-03 17:37:26 +1000

    feat(vue): initial vue stuff

---
+++ InventorySystem.Web.Vue/inventory-system-ui/src/router.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { HomeRoutes } from "@pages/Home/routes";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [...HomeRoutes],
+});
+
+export default router;
+
