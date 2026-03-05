057b5dcf401d34887fdcce9f057a49f5fb842494
Scott <scott.schultz@jotunga.com>
2026-03-05 17:55:51 +1000

    chore: wrangle eslint

--- InventorySystem.Web.Vue/inventory-system-ui/eslint.config.ts
+++ InventorySystem.Web.Vue/inventory-system-ui/eslint.config.ts
@@ -1,6 +1,9 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
+import js from '@eslint/js';
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -8,13 +11,31 @@ import pluginVue from 'eslint-plugin-vue'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{vue,ts,mts,tsx}'],
-  },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs['flat/base'],
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{vue,ts,mts,tsx}'],
+    },
+    {
+        files: ['*.vue', '**/*.vue'],
+        languageOptions: {
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+            },
+        },
+    },
+    {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            '@stylistic/quotes': ['error', 'single'],
+        },
+    },
+    globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  ...pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-)
+    ...pluginVue.configs['flat/essential'],
+    vueTsConfigs.recommended,
+);
--- InventorySystem.Web.Vue/inventory-system-ui/vite.config.ts
+++ InventorySystem.Web.Vue/inventory-system-ui/vite.config.ts
@@ -1,17 +1,17 @@
-import { fileURLToPath, URL } from "node:url";
+import { URL, fileURLToPath } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-            "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
-            "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+            '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
         },
     },
 });
