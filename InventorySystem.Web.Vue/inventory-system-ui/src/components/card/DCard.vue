95b9400d558fc2355de0e5172234c89dd66d82b1
Scott <scott.schultz@jotunga.com>
2026-03-07 08:58:19 +1000

    feat(DCard): update styling for DCard with padding and variant
    - create more useful variable aliases for colours

--- InventorySystem.Web.Vue/inventory-system-ui/src/components/card/DCard.vue
+++ InventorySystem.Web.Vue/inventory-system-ui/src/components/card/DCard.vue
@@ -1,17 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = withDefaults(defineProps<{ padding?: '1' | '2' | '3' | '4' | '5' | '6'; variant?: 'accent' | 'gray' }>(), {
+    variant: 'accent',
+});
+const padding = computed(() => (props.padding ? `p${props.padding}` : undefined));
+</script>
 <template>
-    <div class="card-container">
+    <div class="card-container" :class="variant">
         <slot name="header" />
-        <slot />
+        <div :class="padding">
+            <slot />
+        </div>
     </div>
 </template>
 <style lang="css" scoped>
 .card-container {
-    background-color: var(--component-background-acccent-level-1);
     border-radius: 3px;
-    padding: var(--space-1);
     align-items: baseline;
-    border: 1px solid var(--component-border-acccent-level-1);
+    overflow: hidden;
+}
+
+.accent {
+    background-color: var(--component-background-acccent);
+    border: 1px solid var(--component-border-acccent);
     color: var(--component-font-contrast);
+}
+
+.gray {
+    background-color: var(--component-background-gray);
+    border: 1px solid var(--component-border-gray);
+    color: var(--component-font-contrast);
+}
+
+.p1 {
+    padding: var(--space-1);
+}
+.p2 {
+    padding: var(--space-2);
+}
+.p3 {
+    padding: var(--space-3);
+}
+.p4 {
+    padding: var(--space-4);
+}
+.p5 {
+    padding: var(--space-5);
+}
+.p6 {
+    padding: var(--space-6);
 }
 </style>
